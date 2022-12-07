ee6a065e2ffdc0d9c7af0c9a188fcec9fadad65f
雪中明月 <lswlc33@qq.com>
2022-12-07 23:20:47 +0800

    update

--- Ui_animation.py
+++ Ui_animation.py
@@ -14,7 +14,18 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_animation(object):
     def setupUi(self, animation):
         animation.setObjectName("animation")
-        animation.resize(400, 300)
+        animation.resize(902, 626)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(animation.sizePolicy().hasHeightForWidth())
+        animation.setSizePolicy(sizePolicy)
+        self.label = QtWidgets.QLabel(animation)
+        self.label.setGeometry(QtCore.QRect(110, 150, 670, 490))
+        self.label.setText("")
+        self.label.setPixmap(QtGui.QPixmap(":/img/res/img1.png"))
+        self.label.setScaledContents(True)
+        self.label.setObjectName("label")
 
         self.retranslateUi(animation)
         QtCore.QMetaObject.connectSlotsByName(animation)
@@ -22,3 +33,4 @@ class Ui_animation(object):
     def retranslateUi(self, animation):
         _translate = QtCore.QCoreApplication.translate
         animation.setWindowTitle(_translate("animation", "animation"))
+import res_rc
--- main.py
+++ main.py
@@ -3,16 +3,54 @@ import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QAction, QApplication
 
+
 class animation(QMainWindow, Ui_animation):
     def __init__(self, parent=None):
         super(animation, self).__init__(parent)
         self.setupUi(self)
-        self.geometry()
+        # 设置全屏
+        self.set_screen_size()
+        # 窗口透明和无边框
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WidgetAttribute.WA_TranslucentBackground, True)
+        # 图片定位安放
+        self.pic_loca()
+        self.init_timer()
 
-    def get_geometry(self):
+    def get_screen_size(self):
+        self.desktop = QApplication.desktop()
+        self.screenRect = self.desktop.screenGeometry()
+        self.height = self.screenRect.height()
+        self.width = self.screenRect.width()
+        return self.height, self.width
+
+    def set_screen_size(self):
+        height, width = self.get_screen_size()
+        self.setFixedSize(width, height)
+
+    def pic_loca(self):
+        xx, yy = 670, 490
+        ay, ax = self.get_screen_size()
+        ax = ax/2 - xx/2
+        ay = ay/2 - yy/2
+        self.label.setGeometry(ax, ay, xx, yy)
+
+    def init_timer(self):
+        timer = QtCore.QTimer()
+        timer.timeout.connect(self.if_changed)
+        timer.start(500)
+
+    def if_changed(self):
+
+        if input() == "1":
+            self.init_window()
+            
+    def init_window(self):
+        self.show()
+        
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    myWin = animation()
-    myWin.show()
+    a = animation()
     sys.exit(app.exec_())
+
